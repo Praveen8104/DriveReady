7a4f515645f59233b7c6188502768d03166ba0b0
Praveen8104 <praveenreddygoli8@gmail.com>
2024-07-01 11:40:45 +0530

    Commit

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,12 @@
-var x = [1, 2, 3, 4, 5, 6, 7];
-var max = 0;
-for (let i = 0; i < x.length; i++) {
-    if (x[i] > max)
-        max = x[i]
-}
-console.log(max)
+// var x = [1, 2, 3, 4, 5, 6, 7];
+// var max = 0;
+// for (let i = 0; i < x.length; i++) {
+//     if (x[i] > max)
+//         max = x[i];
+// }
+// console.log(max)
+
+var a = [1, 2, 3, 4, 5, 6, 7]
+a.map(ele => {
+    console.log(ele)
+})
